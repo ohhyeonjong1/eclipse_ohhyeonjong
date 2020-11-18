18b5fa5b9d2e1e45ff8ba91761b1fa40a6f03b8a
오현종 <dhguswhd8469@naver.com>
2020-11-18 20:18:19 +0900

    Print my major (A-7)

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("오현종");
+		System.out.println("컴퓨터소프트웨어공학과");
 	}
 }
  
